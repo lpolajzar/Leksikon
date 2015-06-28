b07762b7e4f352027d45c5b9105f03f13de18c8f
Lucija Polajžar <lucija.polajzar@gmail.com>
2015-06-28 20:51:57 +0200

    Update Vnos.java

--- src/Vnos.java
+++ src/Vnos.java
@@ -1,16 +1,7 @@
 
 import java.io.Serializable;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Lucija
- */
 public class Vnos implements Serializable {
     
     private String ime;
